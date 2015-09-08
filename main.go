794ae84171ab2bd5bc68c62a102aa7dd8059e28e
Rob Nolen <rob.nolen@gmail.com>
2015-09-08 15:36:36 -0700

    hooked up to slack

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
+
 	//"html/template"
+	"bytes"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -10,12 +13,19 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-
+	r.ParseForm()
+	message := []byte(r.FormValue("Message"))
+	api_url := "https://devopsgeekweek.slack.com/services/hooks/slackbot?token=ADywv0e2hv25DM3ea18VylIa"
+	channel := "%23general"
+	log.Println(api_url)
+	resp, _ := http.Post(api_url+"&channel="+channel, "text/html", bytes.NewBuffer(message))
+	response, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(response))
 }
-
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/", http.FileServer(http.Dir("html")))
+	http.HandleFunc("/sendmsg", handler)
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("VCAP_APP_PORT"), nil))
 }
