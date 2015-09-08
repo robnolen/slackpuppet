6fa5c863af6c7efa51440e9364c019c0d2f6e723
Rob Nolen <rob.nolen@gmail.com>
2015-09-08 15:51:42 -0700

    readd main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	//"html/template"
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	//"path"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	message := []byte(r.FormValue("Message"))
+	api_url := "https://devopsgeekweek.slack.com/services/hooks/slackbot?token=" + os.Getenv("SLACK_API_KEY")
+	channel := "%23general"
+	log.Println(api_url)
+	resp, _ := http.Post(api_url+"&channel="+channel, "text/html", bytes.NewBuffer(message))
+	response, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(response))
+}
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/", http.FileServer(http.Dir("html")))
+	http.HandleFunc("/sendmsg", handler)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("VCAP_APP_PORT"), nil))
+}
