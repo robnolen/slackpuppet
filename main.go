47c674c1d8edf72394f463d4cc8f8ff57198001a
Rob Nolen <rob.nolen@gmail.com>
2015-09-08 15:39:53 -0700

    remove api key

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	message := []byte(r.FormValue("Message"))
-	api_url := "https://devopsgeekweek.slack.com/services/hooks/slackbot?token=ADywv0e2hv25DM3ea18VylIa"
+	api_url := "https://devopsgeekweek.slack.com/services/hooks/slackbot?token=" + os.Getenv("SLACK_API_KEY")
 	channel := "%23general"
 	log.Println(api_url)
 	resp, _ := http.Post(api_url+"&channel="+channel, "text/html", bytes.NewBuffer(message))
